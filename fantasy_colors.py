2832b5e1c055c3ee5490047441a6d7b9dd1e6c80
Wes Nicol <wesnicol@me.com>
2021-09-26 23:03:24 -0700

    Updated OAuth2 authorization

--- fantasy_colors.py
+++ fantasy_colors.py
@@ -1,18 +1,33 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+from yahoo_oauth import OAuth2
+from yahoo_fantasy_api import *
+import json
 
 from functions.yahoo_constants import *
 from functions.common_functions import *
 
+
 APP_NAME="fantasy_colors"
+
+
+def getOAuth():
+    log("Getting OAuth2 authentication...")
+    return OAuth2(None, None, from_file=OAUTH_CRED_FILEPATH)
 
 
 def main():
     set_up_log(APP_NAME)
     log("Starting " + APP_NAME + "...")
+    oauth = getOAuth()
+    fantasy_league = league.League(oauth, LEAGUE_ID )
+    print(fantasy_league.current_week())
+
+    league_standings = fantasy_league.standings()
+    print("league_standings: " + str(league_standings))
 
 
-atexit.register(exit_handler)
+#atexit.register(exit_handler)
 if __name__=="__main__":
     main()
---
+++ functions/yahoo_constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding=utf-8
+from functions.common_constants import *
+
+LEAGUE_ID="798555"
+OAUTH_CRED_FILEPATH = PROJECT_HOME + "/keys/yahoo_oauth.json"
+
+
+
+RESOURCES_PATH="resources"
